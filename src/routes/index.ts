b89fc941b7b44feaf77116a00c5ed694cb0bf8c1
Jeong-Taeho <nabi218218@gmail.com>
2023-08-09 17:16:59 +0900

    feat : 페이지 관리

---
+++ src/routes/index.ts
@@ -0,0 +1,11 @@
+import { createRouter, createWebHistory } from "vue-router";
+import MainPage from "./MainPage.vue";
+export default createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      component: MainPage
+    }
+  ]
+});
