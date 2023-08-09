ffec2e46329fba0288d688d7c97bdc9970ac66d5
Jeong-Taeho <nabi218218@gmail.com>
2023-08-09 17:16:45 +0900

    feat : store로 데이터 관리

---
+++ src/store/todos.ts
@@ -0,0 +1,40 @@
+import { defineStore } from "pinia";
+import axios from "axios";
+
+export type Todos = Todo[]; // 할 일 목록
+
+export interface Todo {
+  id: string; // 할 일 ID
+  order: number; // 할 일 순서
+  title: string; // 할 일 제목
+  done: boolean; // 할 일 완료 여부
+  createdAt: string; // 할 일 생성일
+  updatedAt: string; // 할 일 수정일
+}
+
+interface CreateTodoPayload {
+  title: string;
+}
+
+export const useTodosStore = defineStore("todos", {
+  state: () => ({
+    todos: [] as Todos
+  }),
+  getters: {},
+  actions: {
+    async fetchTodos() {
+      const { data } = await axios.post("/api/todos", {
+        method: "GET"
+      });
+      this.todos = data;
+    },
+    async createTodo({ title }: CreateTodoPayload) {
+      await axios.post("/api/todos", {
+        method: "POST",
+        data: {
+          title
+        }
+      });
+    }
+  }
+});
